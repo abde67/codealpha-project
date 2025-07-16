f83bb991ba6ff9ba6f2d1f095ff63b32198a702d
abde7 <abebechehite@gmail.com>
2025-07-16 13:53:45 +0300

    sime js for portffolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -11,3 +11,33 @@ function toggleMenu() {
   closeIcon.style.display = isOpen ? "block" : "none";
 }
 
+  const nav = document.getElementById('mainNav');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      nav.classList.add('nav-scrolled');
+    } else {
+      nav.classList.remove('nav-scrolled');
+    }
+  });
+
+
+   const tabButtons = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    tabButtons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    const tab = button.getAttribute('data-tab');
+    tabContents.forEach(content => {
+      content.classList.remove('active');
+      if (content.id === tab) {
+        content.classList.add('active');
+      }
+    });
+  });
+});
+
+
